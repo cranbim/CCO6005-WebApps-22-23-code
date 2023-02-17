4ba56afd056e93ae8a1fc0af6b30569e325b405b
Dave Webb <dave.webb@cranbim.com>
2023-02-17 09:26:40 +0000

    Create simpler frontend SM

--- Week01 basics/frontend SocialMedia 5 words/code.js
+++ Week01 basics/frontend SocialMedia 5 words/code.js
@@ -1,8 +1,13 @@
 console.log("Simple Posting App")
 
+//Code to retrieve 'user' param from the URL if present
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const userParam = urlParams.get('user')
+
 //A dummy data structure to store data about the user
 const user={
-    userID: 0,
+    userID: userParam,
     someOtherStuff: null
 }
 console.log(user);
@@ -11,7 +16,6 @@ console.log(user);
 let words=document.querySelector('#words')
 words.addEventListener('input',checkWords)
 words.addEventListener('keypress',checkIfReturn)
-let currentWords=''
 
 //get a handle and add event listeners to the 'post' button
 let postButton=document.querySelector('#postwords')
@@ -22,6 +26,11 @@ let recentPosts=[]
 let maxRecents=3
 let nextPostID=0
 
+//variables for the 5 word limit feature
+let maxWords=5
+let currentWords=[]
+let warningSpan=document.querySelector('#warning')
+let warningMessage="only 5 words allowed"
 
 //get a handle on the recent-posts UL
 let recentPostList=document.querySelector("#recent-posts")
@@ -39,8 +48,22 @@ function checkIfReturn(event){
 
 //process input words top limit to 5 words
 function checkWords(event){
-    currentWords=event.target.value
-    console.log(currentWords)
+    currentWords=[]
+    let wordsNow=[]
+    let inputText=event.target.value
+    let inputWords=inputText.split(' ')
+    if(inputWords.length>maxWords){
+        warningSpan.textContent=warningMessage
+        console.log("too many words: maximum 5!")
+        wordsNow=inputWords.slice(0,5)
+        // console.log(wordsNow)
+        words.value=wordsNow.join(' ')
+    } else {
+        warningSpan.textContent=""
+        wordsNow=[...inputWords]
+    }
+    console.log(wordsNow)
+    currentWords=wordsNow
 }
 
 //create dummy data structure for our post, and update recent posts list (database)
@@ -51,7 +74,7 @@ function postWords(){
         let myPost={
             postID: nextPostID++,
             userID: user.userID,
-            post: currentWords,
+            post: [...currentWords],
             likes: 0,
             time: Date.now()
         }
@@ -69,7 +92,7 @@ function postWords(){
 //helper function
 function clearWordInput(){
     words.value=''
-    currentWords=''
+    currentWords=[]
 }
 
 //dummy function to hnadle the posting to the backend if we had one
@@ -82,29 +105,24 @@ function postTheseWords(wordsToPost){
 
 //update the HTML for the recent posts list
 function updateRecentPosts(){
-    //empty the UL of recent posts
     recentPostList.innerHTML=''
     recentPosts.forEach(function(post){
-        // Create a new, empty li
         let li=document.createElement('li')
-        //calculate ellaped mins and secs since post time
         let now=Date.now()
         let ellapsed=new Date(now-post.time)
+        // let ellapsedMins=ellapsed.getMinutes()
         let ellapsedSecs=ellapsed.getSeconds()
         let ellapsedMins=ellapsed.getMinutes()
         ellapsedSecs=String(ellapsedSecs).padStart(2, '0');
-        //create a 'like' button
         let button=document.createElement('button')
         button.textContent='like'
-        button.addEventListener('click',processLike)
-        //add a unique attribute for the like button so it knows which post it belongs to
         button.setAttribute('data-post-id',post.postID.toString())
-        //build the recent post text and add it, with the button, as new li
+        button.addEventListener('click',processLike)
+        let liContent=document.createElement('div')
         let liText=document.createElement('p')
-        liText.textContent=`${post.post} (user ${post.userID}) [${ellapsedMins}:${ellapsedSecs} ago (m:s)] [likes:${post.likes}]`
+        liText.textContent=`${post.post.join(' ')} (user ${post.userID}) [${ellapsedMins}:${ellapsedSecs} ago (m:s)] [likes:${post.likes}]`
         li.appendChild(liText)
         li.appendChild(button)
-        //add the li to the ul
         recentPostList.appendChild(li)
     })
 }
