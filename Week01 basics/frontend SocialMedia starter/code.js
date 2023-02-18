8ddd466ba7f7847ef4d866bc6b6bdbd0632de345
Dave Webb <dave.webb@cranbim.com>
2023-02-18 10:39:21 +0000

    Simplify frontend Sm starter

--- Week01 basics/frontend SocialMedia starter/code.js
+++ Week01 basics/frontend SocialMedia starter/code.js
@@ -1,15 +1,2 @@
 console.log("Simple Posting App")
 
-let postInput=document.querySelector("#words")
-postInput.addEventListener('input',postChanged)
-postInput.addEventListener('keypress', checkIfReturn)
-
-function postChanged(event){
-    console.log(event.target.value)
-}
-
-function checkIfReturn(event){
-    if(event.key === "Enter" || event.which===13){
-        event.preventDefault();
-    }
-}
