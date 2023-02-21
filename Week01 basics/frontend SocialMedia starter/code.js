8324e36ae56f5f59f7ad863386e4ce09bdbeffa3
Dave Webb <dave.webb@cranbim.com>
2023-02-21 12:50:45 +0000

    setup week 2 backend

--- Week01 basics/frontend SocialMedia starter/code.js
+++ Week01 basics/frontend SocialMedia starter/code.js
@@ -1,2 +1,50 @@
 console.log("Simple Posting App")
 
+const userID=1
+let nextPostId=0
+let recentPosts=[]
+
+let postWords=document.getElementById('words')
+let postButton=document.querySelector('#post-button')
+let recentPostsList=document.querySelector('#recent-posts')
+
+postWords.addEventListener('keypress', processKeys)
+postWords.addEventListener('input', postDidChange)
+postButton.addEventListener('click', processPostButton)
+
+function processKeys(event){
+    if(event.key === "Enter" || event.which===13){
+        createPost(event.target.value)
+        event.preventDefault()
+    }
+}
+
+function processPostButton(event){
+    createPost(postWords.value)
+}
+
+function postDidChange(event){
+    console.log(event.target.value)
+}
+
+function createPost(postText){
+    let post={
+        content: postText,
+        postId: nextPostId++,
+        timePosted: Date.now(),
+        user: userID
+    }
+    postWords.value=''
+    console.log(post)
+    recentPosts.unshift(post)
+    updatePostsList()
+}
+
+function updatePostsList(){
+    recentPostsList.textContent=''
+    recentPosts.forEach(function(post,i){
+        let postLi=document.createElement('li')
+        postLi.textContent=`${post.content} by user ${post.user} post id=${post.postId}`
+        recentPostsList.appendChild(postLi)
+    })
+}
---
+++ Week02 Node and Express/SocialMediaSim with backend/index.js
@@ -0,0 +1,31 @@
+const express=require('express')
+const app = express()
+app.listen(3000, ()=> console.log('nano listening at port 3000'))
+app.use(express.static('public'))
+app.use(express.json())
+
+const postList=[]
+let nextPostID=0
+defaultQtyPostsToSend=3
+
+
+app.post('/newpost', (request, response)=>{
+    console.log(request.body)
+    let post=request.body
+    post.postID=nextPostID++
+    postList.unshift(post)
+    response.json({
+        posts:postList.slice(0,defaultQtyPostsToSend)
+    })
+    
+})
+
+app.get('/getposts', (request, response)=>{
+    // console.log(request.body)
+    const qtyPosts = request.query.qty;
+    console.log('send me '+qtyPosts+' posts please')
+    response.json({
+        posts:postList.slice(0,qtyPosts)
+    })
+})
+
