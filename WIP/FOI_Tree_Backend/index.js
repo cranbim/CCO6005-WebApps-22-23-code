99dd7bccbac63ec7bd9aff21d769c041d030992a
Dave Webb <dave.webb@cranbim.com>
2023-04-23 11:55:53 +0100

    Week 8 for videos

---
+++ WIP/FOI_Tree_Backend/index.js
@@ -0,0 +1,47 @@
+const { MongoClient, ObjectID } = require("mongodb");
+const Express = require("express");
+
+const BodyParser = require('body-parser');
+
+const server = Express();
+
+server.use(BodyParser.json());
+server.use(BodyParser.urlencoded({ extended: true }));
+
+require('dotenv').config();
+const mongoDBconnectionString=process.env.MYMONGODBURI
+console.log("!!!"+mongoDBconnectionString)
+const client = new MongoClient(mongoDBconnectionString);
+
+var collection;
+
+// schema
+// tree: name (string)
+// colour: [int,int,int]
+// size: int
+// type: int
+
+// server.post("/plummies", async (request, response, next) => {});
+// server.get("/plummies", async (request, response, next) => {});
+// server.get("/plummies/:id", async (request, response, next) => {});
+// server.put("/plummies/:plummie_tag", async (request, response, next) => {});
+
+async function writeSomething(data){
+    let result = await collection.insertOne(data);
+    console.log(result)
+}
+
+server.listen("3000", async () => {
+    try {
+        await client.connect();
+        collection = client.db("FOI2023").collection("trees01");
+        console.log("Listening at :3000...");
+        await writeSomething({
+            treeName: "I am Groot",
+            colour: "red",
+            size: 3
+        })
+    } catch (e) {
+        console.error(e);
+    }
+});
